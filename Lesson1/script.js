dc51ff092e6015f2b22070ffaeba45c5f56db26a
alzhukov <cmexprox@vk.com>
2022-08-19 18:52:13 +0300

    JS First lesson HW

---
+++ Lesson1/script.js
@@ -0,0 +1,40 @@
+//var - global
+//let - local
+//const - let unchangable
+
+//console.log('hellow hello');
+
+/**
+ * function summary
+ * @param {Number} a
+ * @param {Number} b
+ * @returns
+ */
+//function sum(a, b) {
+//return a + b;
+//}
+
+//primitivs
+//String
+//let a = 'asd';
+//let b = "qwe";
+//let name = 'zxc';
+//console.log(`Hi ${name}`);
+//console.log('sdfsdffs \n sdfsfd');
+//console.log('sdfsdfdsf sdfdsfdsf \
+//sdfsdf ssdfs dfs');
+
+//Number
+//let a = 1;
+//let b = 3.14;
+//a = +a; string to number
+//a=Number(a);
+//a = a +''; number to string
+//parseInt() parseFloat();
+
+//'use strict';
+//a = 10;
+//console.log(a);
+
+//let a = 1;
+//a = a + 2; a += 2;
